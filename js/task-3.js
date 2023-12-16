e9ebc98f0ebefd53804c268a8b88e3095b66cfe2
Mr.Zombie <scion8337@gmail.com>
2023-12-16 22:21:59 +0200

    improve variable naming

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function checkForSpam(message) {
-    const messageInTheLowerCase = message.toLowerCase();
-    return messageInTheLowerCase.includes("spam") || messageInTheLowerCase.includes("sale");
+    const lowercasedMessage = message.toLowerCase();
+    return lowercasedMessage.includes("spam") || lowercasedMessage.includes("sale");
 }
 
 console.log(checkForSpam("Latest technology news")); // false
